3c63e560b50ac106bb0a393f499da21e2fb6645a
Asdfg6543 <nhatanh2221995@gmail.com>
2022-05-10 10:08:20 +0700

    Enter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import Signup from "./buttons/Login/Signup";
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light row">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid py-2">
           <button
             className="navbar-toggler"
@@ -20,7 +20,7 @@ const Header = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse md-col-6" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink className="nav-link active" aria-current="page" to="/">
